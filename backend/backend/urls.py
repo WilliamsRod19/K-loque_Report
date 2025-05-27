fafe3ddb6e035bbcc4cc8beb5efedd4e1c3276ea
Neith21 <cristian.1945theend@gmail.com>
2025-05-26 21:45:24 -0600

    Implementación de documentación en Swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,10 +24,26 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
+schema_view=get_schema_view(
+    openapi.Info(
+        title="K-loque Report",
+        default_version='v1',
+        description="Api desarrollada para implementación de backend de un sistema de reportes, para manejo en cualquier frontend",
+        terms_of_service="https://us-tuna-sounds-images.voicemod.net/adb699f3-cf85-4c5d-a74b-407d789a1c6a-1679070395042.jpg",
+        contact=openapi.Contact(email="cristian.1945theend@gmail.com"),
+        license=openapi.License(name="BSD License")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('user_control.urls')),
     path('api/v1/', include('incident.urls')),
+    path('documentation<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('documentation/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
